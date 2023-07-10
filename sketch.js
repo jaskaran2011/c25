d97a6a5ed7f913eb05d5311e36019fe5353754bd
jaskaran2011 <133873025+jaskaran2011@users.noreply.github.com>
2023-07-10 18:17:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var g1,g2,g3
+var btn2;
+
+var angle=60;
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+   btn2 = createImg('up.png');
+  btn2.position(350,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  ground1 = Bodies.rectangle(100,300,100,20,ground_options);
+  World.add(world,ground1);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  g1=new Ground(50,370,50,30);
+  g2=new Ground(150,370,50,30);
+  g3=new Ground(250,370,50,30);
+ 
+  ground = Bodies.rectangle(100,400,650,20,ground_options);
+  World.add(world,ground); 
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+  Matter.Body.rotate(ground1,angle)
+ push();
+  translate(ground1.position.x,ground1.position.y);
+  rotate(angle);
+  rect(0,0,100,20);
+  pop();
+  
+  angle +=0.1;
+  g1.display();
+  g2.display();
+  g3.display();
+ 
+
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,650,20);
+  
+//console.log(ground.position.y);
+
+  
+  
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+  
